5362f5fa77cddcc38590fdbc72705ebb0f27f130
Hossein Keshtkar Haranaki <rock.hkh.g1@gmail.com>
2021-08-10 01:47:56 +0430

    update

--- JavaScript.js
+++ JavaScript.js
@@ -2,7 +2,8 @@ const btn = document.querySelector('.btn');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModal = document.querySelector('.closeModal');
-const hiddenRemov = function() {
+
+const hiddenRemove = function() {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
@@ -10,6 +11,11 @@ const hiddenAdd = function() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 };
-btn.addEventListener('click', hiddenRemov);
+btn.addEventListener('click', hiddenRemove);
 closeModal.addEventListener('click', hiddenAdd);
 overlay.addEventListener('click', hiddenAdd);
+document.addEventListener('keydown', function (keyboard) {
+    if (keyboard.key === 'Escape' && !modal.classList.contains('hidden')) {
+        hiddenAdd();
+    }
+});
